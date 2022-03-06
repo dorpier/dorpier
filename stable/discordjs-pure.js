5f9bc2f31bba290fe084c6da0022cb519e09535f
1305 <1305@national.shitposting.agency>
2022-03-06 19:14:48 +0000

    Update discordjs-pure.js

--- stable/discordjs-pure.js
+++ stable/discordjs-pure.js
@@ -4,8 +4,8 @@
 function client() {} // ez way to get ez funcs like client.send_message(), etc
 
 client.get_token = function() {
-  let GLOBAL_USER_TOKEN = window.localStorage.getItem('token').replace("\"", "").replace("\"", "");
-  return GLOBAL_USER_TOKEN;
+    let GLOBAL_USER_TOKEN = window.localStorage.getItem('token').replace("\"", "").replace("\"", "");
+    return GLOBAL_USER_TOKEN;
 }
 
 let GLOBAL_USER_TOKEN = client.get_token();
@@ -22,16 +22,16 @@ client.send_message = function(message, chan_id, token) { // send message func, 
     }
     if (token && message && chan_id) {
         let post_url = `https://discord.com/api/v9/channels/${chan_id}/messages`;
-            let request = new XMLHttpRequest();
-            request.withCredentials = true;
-            request.open("POST", post_url);
-            request.setRequestHeader("authorization", token);
-            request.setRequestHeader("accept", "/");
-            request.setRequestHeader("authority", "discordapp.com");
-            request.setRequestHeader("content-type", "application/json");
-            request.send(JSON.stringify({
-                content: message
-            }));
+        let request = new XMLHttpRequest();
+        request.withCredentials = true;
+        request.open("POST", post_url);
+        request.setRequestHeader("authorization", token);
+        request.setRequestHeader("accept", "/");
+        request.setRequestHeader("authority", "discordapp.com");
+        request.setRequestHeader("content-type", "application/json");
+        request.send(JSON.stringify({
+            content: message
+        }));
     } else {
         console.warn("[discordjs-pure] message send aborted!");
     }
@@ -39,8 +39,8 @@ client.send_message = function(message, chan_id, token) { // send message func, 
 }
 
 client.delete_message = function(msg, token) { // send message func, very ez
-  var chid = msg.channel_id;
-  var msgid = msg.id;
+    var chid = msg.channel_id;
+    var msgid = msg.id;
     if (!token) {
         console.warn("[discordjs-pure] no token was provided. aborting message del!");
     }
@@ -49,14 +49,14 @@ client.delete_message = function(msg, token) { // send message func, very ez
     }
     if (token && msg) {
         let del_url = `https://discord.com/api/v9/channels/${chid}/messages/${msgid}`;
-            let request = new XMLHttpRequest();
-            request.withCredentials = true;
-            request.open("DELETE", del_url);
-            request.setRequestHeader("authorization", token);
-            request.setRequestHeader("accept", "/");
-            request.setRequestHeader("authority", "discordapp.com");
-            request.setRequestHeader("content-type", "application/json");
-            request.send(null);
+        let request = new XMLHttpRequest();
+        request.withCredentials = true;
+        request.open("DELETE", del_url);
+        request.setRequestHeader("authorization", token);
+        request.setRequestHeader("accept", "/");
+        request.setRequestHeader("authority", "discordapp.com");
+        request.setRequestHeader("content-type", "application/json");
+        request.send(null);
     } else {
         console.warn("[discordjs-pure] message del aborted!");
     }
@@ -64,49 +64,59 @@ client.delete_message = function(msg, token) { // send message func, very ez
 }
 
 client.run = function(token) {
-  var ws = new WebSocket("wss://gateway.discord.gg/?v=6&encoding=json");
-var interval = 0;
+    var ws = new WebSocket("wss://gateway.discord.gg/?v=6&encoding=json");
+    var interval = 0;
 
-payload = {
-   op: 2,
-   d: {
-      token: token,
-      intents: 512,
-      properties: {
-         $os: "linux",
-         $browser: "chrome",
-         $device: "chrome",
-      },
-   },
-};
+    payload = {
+        op: 2,
+        d: {
+            token: token,
+            intents: 512,
+            properties: {
+                $os: "linux",
+                $browser: "chrome",
+                $device: "chrome",
+            },
+        },
+    };
 
-ws.addEventListener("open", function open(x) {
-   ws.send(JSON.stringify(payload));
-});
+    ws.addEventListener("open", function open(x) {
+        ws.send(JSON.stringify(payload));
+    });
 
-ws.addEventListener("message", function incomming(data) {
-   var x = data.data;
-   var payload = JSON.parse(x);
+    ws.addEventListener("message", function incomming(data) {
+        var x = data.data;
+        var payload = JSON.parse(x);
 
-   const { t, event, op, d } = payload;
+        const {
+            t,
+            event,
+            op,
+            d
+        } = payload;
 
-   switch (op) {
-      // OPCODE 10 GIVES the HEARTBEAT INTERVAL, SO YOU CAN KEEP THE CONNECTION ALIVE
-      case 10:
-         const { heartbeat_interval } = d;
-         setInterval(() => {
-            ws.send(JSON.stringify({ op: 2, d: null }));
-         }, heartbeat_interval);
+        switch (op) {
+            // OPCODE 10 GIVES the HEARTBEAT INTERVAL, SO YOU CAN KEEP THE CONNECTION ALIVE
+            case 10:
+                const {
+                    heartbeat_interval
+                } = d;
+                setInterval(() => {
+                    ws.send(JSON.stringify({
+                        op: 2,
+                        d: null
+                    }));
+                }, heartbeat_interval);
 
-         break;
-   }
+                break;
+        }
 
-   switch (t) {
-      // IF MESSAGE IS CREATED, IT WILL LOG IN THE CONSOLE
-      case "MESSAGE_CREATE":
-         client.on_message(d, GLOBAL_USER_TOKEN);
-   }
-});
+        switch (t) {
+            // IF MESSAGE IS CREATED, IT WILL LOG IN THE CONSOLE
+            case "MESSAGE_CREATE":
+                client.on_message(d, GLOBAL_USER_TOKEN);
+        }
+    });
 }
 
 client.run(GLOBAL_USER_TOKEN);
