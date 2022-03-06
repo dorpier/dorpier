b5896143b438a3830dabf57540cea9529749d094
1305 <1305@national.shitposting.agency>
2022-03-06 16:40:24 +0000

    Update discordjs-pure.js

--- stable/discordjs-pure.js
+++ stable/discordjs-pure.js
@@ -1,6 +1,16 @@
-// START.
-function client() {}
-client.send_message = function(message, chan_id, token) {
+// JAVASCRIPT START.
+
+/* globals & funcs stuff */
+function client() {} // ez way to get ez funcs like client.send_message(), etc
+
+client.get_token = function() {
+  let GLOBAL_USER_TOKEN = window.localStorage.getItem('token').replace("\"", "").replace("\"", "");
+  return GLOBAL_USER_TOKEN;
+}
+
+let GLOBAL_USER_TOKEN = client.get_token();
+
+client.send_message = function(message, chan_id, token) { // send message func, very ez
     if (!token) {
         console.warn("[discordjs-pure] no token was provided. aborting message send!");
     }
@@ -12,8 +22,6 @@ client.send_message = function(message, chan_id, token) {
     }
     if (token && message && chan_id) {
         let post_url = `https://discord.com/api/v9/channels/${chan_id}/messages`;
-        let once = 1;
-        for (let i = 0; i < once; i++) {
             let request = new XMLHttpRequest();
             request.withCredentials = true;
             request.open("POST", post_url);
@@ -24,41 +32,68 @@ client.send_message = function(message, chan_id, token) {
             request.send(JSON.stringify({
                 content: message
             }));
-        }
     } else {
         console.warn("[discordjs-pure] message send aborted!");
     }
 
 }
+/*
+client.parsemessage = function(d, token) {
+  let content = d.content.trim();
+  if (d.author.id.toString() != "569675586541191346"){
+    return;
+  }
+  else{
+    if (content == "1305.test.like.5000") {
+      client.send_message("i just received & parsed a discord message in pure, browser-based javascript!", "876658708149571595", token);
+    }
+  }
+}
+*/
+client.run = function(token) {
+  var ws = new WebSocket("wss://gateway.discord.gg/?v=6&encoding=json");
+var interval = 0;
 
-client.spam_message = function(message, amount, chan_id, token) {
-    if (!message) {
-        console.warn("[discordjs-pure] no message was provided. aborting message spam!");
-    }
-    if (!amount) {
-        console.warn("[discordjs-pure] no amount was provided. aborting message spam!");
-    }
-    if (!chan_id) {
-        console.warn("[discordjs-pure] no channel id was provided. aborting message spam!");
-    }
-    if (!token) {
-        console.warn("[discordjs-pure] no token was provided. aborting message spam!");
-    }
-    if (message && amount && chan_id && token) {
-        let post_url = `https://discord.com/api/v9/channels/${chan_id}/messages`;
-        for (let i = 0; i < amount; i++) {
-            let request = new XMLHttpRequest();
-            request.withCredentials = true;
-            request.open("POST", post_url);
-            request.setRequestHeader("authorization", token);
-            request.setRequestHeader("accept", "/");
-            request.setRequestHeader("authority", "discordapp.com");
-            request.setRequestHeader("content-type", "application/json");
-            request.send(JSON.stringify({
-                content: message
-            }));
-        }
-    }
+payload = {
+   op: 2,
+   d: {
+      token: token,
+      intents: 512,
+      properties: {
+         $os: "linux",
+         $browser: "chrome",
+         $device: "chrome",
+      },
+   },
+};
+
+ws.addEventListener("open", function open(x) {
+   ws.send(JSON.stringify(payload));
+});
+
+ws.addEventListener("message", function incomming(data) {
+   var x = data.data;
+   var payload = JSON.parse(x);
+
+   const { t, event, op, d } = payload;
+
+   switch (op) {
+      // OPCODE 10 GIVES the HEARTBEAT INTERVAL, SO YOU CAN KEEP THE CONNECTION ALIVE
+      case 10:
+         const { heartbeat_interval } = d;
+         setInterval(() => {
+            ws.send(JSON.stringify({ op: 2, d: null }));
+         }, heartbeat_interval);
+
+         break;
+   }
+
+   switch (t) {
+      // IF MESSAGE IS CREATED, IT WILL LOG IN THE CONSOLE
+      case "MESSAGE_CREATE":
+         client.parsemessage(d, GLOBAL_USER_TOKEN);
+   }
+});
 }
 
-// END.
+client.run(GLOBAL_USER_TOKEN);
