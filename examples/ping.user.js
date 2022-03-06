b1218092746fb46a2211397d7085ae169675a15d
1305 <1305@national.shitposting.agency>
2022-03-06 19:16:52 +0000

    Create ping.user.js

---
+++ examples/ping.user.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name        testing discord.js-pure
+// @match       https://discord.com/*
+// @grant       none
+// @version     1.0
+// @author      1305
+// @require https://raw.githubusercontent.com/13-05/discord.js-pure/main/stable/discordjs-pure.js
+// ==/UserScript==
+
+
+let GLOBAL_USER_TOKEN = client.get_token();
+
+
+client.on_message = function(message, token) {
+  let content = message.content.trim(); // sometimes the message content has whitespace at the end, so we just say message.content.trim() is == to message.content
+  if (message.author.id.toString() != "YOURDISCORDUSERID") { // put your userid here!!!! <--------
+    return;
+  }
+  else{
+    if (content == "ping"){
+      client.send_message("pong!", message.channel_id, token); // i looked at the "message" object and that's the path of a channelid; now it'll respond ez!
+      client.delete_message(message, token);
+    }
+  }
+}
+
+client.run(GLOBAL_USER_TOKEN);
