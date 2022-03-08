e3e65d4a3202eea443a0d2aa914e955c36d4e482
1305 <1305@national.shitposting.agency>
2022-03-08 22:46:02 +0000

    maaaaaybe this'll make bots work?
    you now have to declare bot tokens starting with `let token = "Bot <TOKENHEREEZ>";`, though.

--- stable/discordjs-pure.js
+++ stable/discordjs-pure.js
@@ -49,17 +49,30 @@ client.send_message = function(message, chan_id, token) { // send message func, 
         console.warn("[discordjs-pure] no channel id was specified, aborting message send!");
     }
     if (token && message && chan_id) {
-        let post_url = `https://discord.com/api/v9/channels/${chan_id}/messages`;
-        let request = new XMLHttpRequest();
-        request.withCredentials = true;
-        request.open("POST", post_url);
-        request.setRequestHeader("authorization", token);
-        request.setRequestHeader("accept", "/");
-        request.setRequestHeader("authority", "discordapp.com");
-        request.setRequestHeader("content-type", "application/json");
-        request.send(JSON.stringify({
-            content: message
-        }));
+        if (token.startsWith("Bot")){
+          let post_url = `https://discord.com/api/v9/channels/${chan_id}/messages`;
+          let request = new XMLHttpRequest();
+          request.withCredentials = true;
+          request.open("POST", post_url);
+          request.setRequestHeader("authorization", token);
+          request.setRequestHeader("content-type", "application/json");
+          request.send(JSON.stringify({
+              content: message
+          }));
+        }
+        else {
+          let post_url = `https://discord.com/api/v9/channels/${chan_id}/messages`;
+          let request = new XMLHttpRequest();
+          request.withCredentials = true;
+          request.open("POST", post_url);
+          request.setRequestHeader("authorization", token);
+          request.setRequestHeader("accept", "/");
+          request.setRequestHeader("authority", "discordapp.com");
+          request.setRequestHeader("content-type", "application/json");
+          request.send(JSON.stringify({
+              content: message
+          }));
+        }
     } else {
         console.warn("[discordjs-pure] message send aborted!");
     }
