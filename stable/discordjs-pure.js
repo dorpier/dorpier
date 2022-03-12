312296d1bcdeb3db7e287ff2af425506eb427b64
1305 <1305@national.shitposting.agency>
2022-03-12 02:18:42 +0000

    Update discordjs-pure.js

--- stable/discordjs-pure.js
+++ stable/discordjs-pure.js
@@ -15,7 +15,9 @@ var Discord = {
     experimental: {
       staff_mode: {
         enable: function(){
-          Object.defineProperty((webpackChunkdiscord_app.push([[''],{},e=>{m=[];for(let c in e.c)m.push(e.c[c])}]),m).find(m=>m?.exports?.default?.isDeveloper!==void 0).exports.default,"isDeveloper",{get:()=>true, configurable:true}); 
+          //Object.defineProperty((webpackChunkdiscord_app.push([[''],{},e=>{m=[];for(let c in e.c)m.push(e.c[c])}]),m).find(m=>m?.exports?.default?.isDeveloper!==void 0).exports.default,"isDeveloper",{get:()=>true, configurable:true});
+          var findModule=(item)=>window.webpackChunkdiscord_app.push([[Math.random()],{},(req)=>{for(const m of Object.keys(req.c).map((x)=>req.c[x].exports).filter((x)=>x)){if(m.default&&m.default[item]!==undefined)return m.default}}])
+          findModule("isDeveloper").isDeveloper = true; 
         },
         disable: function(){
           Object.defineProperty((webpackChunkdiscord_app.push([[''],{},e=>{m=[];for(let c in e.c)m.push(e.c[c])}]),m).find(m=>m?.exports?.default?.isDeveloper!==void 0).exports.default,"isDeveloper",{get:()=>false, configurable:true}); 
