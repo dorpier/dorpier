ed3dca1135d0e0f229f67213c209d8223754b9d0
1305 <1305@national.shitposting.agency>
2022-03-16 14:29:30 -0500

    Update ping.user.js

--- examples/ping.user.js
+++ examples/ping.user.js
@@ -1,26 +1,27 @@
+javascript
 // ==UserScript==
-// @name        testing discord.js-pure
-// @match       https://discord.com/*
+// @name        Ping-SB
+// @match       *://discord.com/*
 // @grant       none
-// @version     1.0
-// @author      1305
+// @version     0.1
 // @require https://raw.githubusercontent.com/13-05/discord.js-pure/main/stable/discordjs-pure.js
+// @author      1305
+// @description A 'ping' selfbot written in the pure javascript version of discord.js!
+// @run-at document-start
 // ==/UserScript==
-
-
-let GLOBAL_USER_TOKEN = client.get_token();
-let USERID = client.get_userid(GLOBAL_USER_TOKEN);
-
-client.on_message = function(message, token) {
-  let content = message.content.trim(); // sometimes the message content has whitespace at the end, so we just say message.content.trim() is == to message.content
-  if (message.author.id.toString() != USERID) {
-    return;
-  }
-  else{
-    if (content == "ping"){
-      client.send_message("pong!", message.channel_id, token); // i looked at the "message" object and that's the path of a channelid; now it'll respond ez!
-    }
-  }
-}
-
-client.run(GLOBAL_USER_TOKEN);
+(function() {
+    'use strict';
+  Discord.experimental.amoled_dark_mode.enable();
+  let token = client.get_token();
+  var userid = client.get_userid(token);
+  let PREFIX = prompt("prefix: ");
+  userid.then(userid => {
+     client.on_message=function(message,token){
+       if (message.author.id.toString() != userid) return;
+       if (message.content.trim() == `${PREFIX}ping`){
+         client.send_message("pong!", message.channel_id, token);
+       }
+     }
+  });
+  client.run(token);
+})();
