2bd326e4cf36554a5979c8e874f546014939cb60
1305 <1305@national.shitposting.agency>
2022-03-16 20:49:58 +0000

    Update discordjs-pure.js

--- stable/discordjs-pure.js
+++ stable/discordjs-pure.js
@@ -1,5 +1,5 @@
 let KEEPLOGS = true;
-Discord.Logger.Log(`NOTE: By default, Discord.JS-Pure logs events. Run 'Discord.Logger'`);
+Discord.Logger.Log(`NOTE: By default, Discord.JS-Pure logs events. Run 'Discord.Logger.disable();' to disable the logs if they get too intrusive.`);
 
 var Discord = {
     sleep: function(milliseconds) {
