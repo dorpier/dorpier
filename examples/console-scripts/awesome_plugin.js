520091326201f7df772906c1d2200ed2880733e6
1305 <1305@national.shitposting.agency>
2022-04-13 21:49:36 +0000

    Create awesome_plugin.js

---
+++ examples/console-scripts/awesome_plugin.js
@@ -0,0 +1,20 @@
+Utils.plugins.add({
+    name: "myAwesomePlugin",
+    start: function() {
+        console.log("Started My Awesome Plugin!");
+    },
+    stop: function() {
+        console.log("Stopped My Awesome Plugin!");
+    }
+});
+
+/*
+Utils.plugins.start("myAwesomePlugin");
+
+- to start a plugin
+*/
+/*
+Utils.plugins.stop("myAwesomePlugin");
+
+- to stop a plugin
+*/
